36e268c98402c690b5c2eaa17086276f2bb66cc8
Vincent <3d314831994@gmail.com>
2022-06-24 01:00:29 +1000

    Modified Character animations

--- Assets/Characters/Scripts/SkillTester.cs
+++ Assets/Characters/Scripts/SkillTester.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[System.Serializable]
+public struct SkillContainer{
+    public enum TriggerAnimation{
+        None,
+        Beginning,
+        First,
+        Second
+    }
+    public VFX_BaseSkill PF_Skill;
+    public TriggerAnimation cancelTrigger;    
+}
+
 public class SkillTester : MonoBehaviour
 {
-    public VFX_BaseSkill skillToTest;
+    public SkillContainer[] skillToTest;
+    private int skillIndex;
     [SerializeField]
     private Transform skillPoint;
     private Animator anim;
@@ -16,7 +30,7 @@ public class SkillTester : MonoBehaviour
     }
     void Start()
     {
-        
+        skillIndex = 0;
     }
 
     void Update()
@@ -24,13 +38,28 @@ public class SkillTester : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space)){
            CreateSkillVFX();
         }
-
+        if(Input.GetKeyDown(KeyCode.Q) && skillIndex > 0){
+            skillIndex -= 1;
+        }
+         if(Input.GetKeyDown(KeyCode.E) && skillIndex < skillToTest.Length - 1){
+            skillIndex += 1;
+        }
+        anim.SetInteger("SkillIndex",skillIndex);
         anim.SetBool("isCasting",isCastingSkill);
     }
     
     void CreateSkillVFX(){
         isCastingSkill = true;
-        VFX_BaseSkill currentVFX = Instantiate(skillToTest,skillPoint.position,Quaternion.identity);
-        currentVFX.LaunchSkill(() => isCastingSkill = false);
+
+        VFX_BaseSkill currentVFX = Instantiate(skillToTest[skillIndex].PF_Skill,skillPoint.position,Quaternion.identity);
+        switch (skillToTest[skillIndex].cancelTrigger)
+        {
+            case SkillContainer.TriggerAnimation.First:
+            currentVFX.LaunchSkill(() => isCastingSkill = false);
+            break;
+            case SkillContainer.TriggerAnimation.Second:
+            currentVFX.LaunchSkill(null,() => isCastingSkill = false);
+            break;
+        }
     }
 }
